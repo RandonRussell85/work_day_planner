96642396c7cac084a029cf34d0df02d0c980ed0f
Randon Russell <randonrussell@ymail.com>
2021-05-26 21:24:45 -0700

    updated README, also wrote more html and added more js

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -2,4 +2,61 @@ var textAreaContent = {
 
 };
 
+ var header = document.header;
+ const m = moment();
+ var m2 = m.format("dddd, MMMM Do")
+ var m3 = m.format("YYYY")
 
+    $(currentDay).text(m2)
+    $(currentYear).text(m3)
+
+ $(".info").each(function(){
+     var currentTime = moment().hours();
+
+     var currentHour = Number(currentTime);
+     console.log(currentHour)
+
+     var pId = $(this).children("div").children("p").attr("id");
+     var taskTime = Number(pId);
+     console.log("task time as a number", taskTime);
+
+     if(taskTime < currentHour){
+        $(this).children("textarea").addClass('past')
+      }
+    
+      else if(taskTime === currentHour) {
+        $(this).children("textarea").addClass('present')
+      }
+
+      else{
+        $(this).children("textarea").addClass('future')}
+    
+     });
+
+   
+ $(".saveBtn").on("click", function() {
+    var targetTextArea = $(this).parent('.info').children('textarea')
+   
+    var textAreaId =  targetTextArea.attr('id')
+    
+    localStorage.setItem("task-"+ textAreaId ,targetTextArea.val());
+     
+  });
+  
+   function getLocalStorage() {
+  if (localStorage.getItem("textarea")){
+    textAreaContent = JSON.parse(localStorage.getItem('textarea'));
+    }
+   }
+  
+    function setTextContent (){
+      for (const [key, value] of Object.entries(textAreaContent)){
+        $("#"+key).val(value);
+      }
+    }
+  
+  $(document).ready(function(){
+    getLocalStorage();
+    setTextContent();
+  })
+ 
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# work_day_planner
-making a work day planner for homework
+# Work Day Scheduler
+
+## Goal
+Object is to create a schedule app for a person's work day. In the app the user will be able to schedule events every hour and save them to the Day Scheduler. The scheduler will then color code the events by letting them know what one is current based on the current time.
+
+- When the planner is opened the current day and time is displayed at the top.
+- The planner contains time blocks with standard business hours
+- Each time block is color coded to indicate if it is in the past, present, or future
+- When the user clicks into the time block they can enter an event
+- Once the event is entered the user clickes the "save" button and the event is saved to local storage
+- when the page is refreshed the events are still in the time blocks
